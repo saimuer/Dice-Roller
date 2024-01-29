23ba4a0155ad4bc7bb99736f8973ca240d3ef787
塞缪尔 <122048491+saimuer@users.noreply.github.com>
2024-01-29 07:34:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function rollDice() {
     for(let i = 0; i < numOfDice; i++) {
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
-        images.push(`<img src="${value}.png">`);
+        images.push(`<img src="1/${value}.png">`);
     }
 
     diceResult.textContent = `dice: ${values.join(', ')}`;
